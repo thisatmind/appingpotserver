f5cfd65ce9af0f7c5b52eb961109e97807b797ee
Patrick Shim <nasuk0610@gmail.com>
2016-10-26 19:20:25 +0900

    add gulp babel

---
+++ gulpfile.babel.js
@@ -0,0 +1,19 @@
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import nodemon from 'gulp-nodemon';
+
+gulp.task('build', () =>
+    gulp.src('src/app.js')
+        .pipe(babel({
+            plugins: ['transform-runtime']
+        }))
+        .pipe(gulp.dest('dist'))
+);
+
+gulp.task('server', ['build'], () => {
+    nodemon({
+        script: './dist/app.js',
+        watch: false,
+        env: {'NODE_ENV': 'production'}
+    })
+});
---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.listen('8080', (req, res, next) => {
+   console.log('server is listening 8080'); 
+});
