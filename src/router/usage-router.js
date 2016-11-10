cacc5de362887454ded21e053587e3eafe94b432
Patrick Shim <nasuk0610@gmail.com>
2016-11-10 05:19:26 +0000

    bug fix

--- src/router/usage-router.js
+++ src/router/usage-router.js
@@ -6,11 +6,3 @@ const router = Router();
 router.post('/', UsageController.add);
 
 export default router;
-import {Router} from 'express';
-import UsageController from './../controller/usage-controller';
-
-const router = Router();
-
-router.post('/', UsageController.add);
-
-export default router;
