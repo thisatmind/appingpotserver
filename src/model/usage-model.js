cec55277087dac5e36821edcf4dffb40523aae64
Patrick Shim <nasuk0610@gmail.com>
2016-11-10 14:15:44 +0000

    update query

--- src/model/usage-model.js
+++ src/model/usage-model.js
@@ -3,8 +3,7 @@ import pool from '../db';
 const model = {
     addUsage: (usageList) => {
       
-      const stmt = "INSERT INTO raw_app_usage VALUES (?,?,?,?)"
-      let query = "";
+      let query = "INSERT INTO raw_app_usage VALUES";
       const options = [];
       
       console.log(usageList);
@@ -13,12 +12,10 @@ const model = {
         options.push(data.userId);
         options.push(data.date);
         options.push(data.usage);
-        query = `${query}${stmt},`;
+        query = `${query}(?,?,?,?),`;
       });
       
       query = `${query.substr(0, query.length -1)};`;
-      console.log(query);
-      console.log(options);
       return pool.query(query, options);
     }
 };
