c9b937d4b0e617a180c5a6011e60e02bb8af8ac8
Patrick Shim <nasuk0610@gmail.com>
2016-11-13 04:44:01 +0000

    update router

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import user from './router/user-router';
 import usage from './router/usage-router';
 import FirebaseManager from './firebase-util';
 import install from './router/install-router';
+import count from './router/count-router';
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(bodyParser.json())
 // express router
 app.use('/user', user);
 app.use('/usage', usage);
+app.use('/count', count);
 app.use('/install', install);
 
 const port = process.env.PORT || 8080;
